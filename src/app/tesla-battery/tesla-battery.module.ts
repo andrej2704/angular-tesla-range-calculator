f1c54157ed3f3463749d7f9ebcc6184b81871065
basementcz <you@example.com>
2016-12-19 07:12:05 +0100

    added first component

---
+++ src/app/tesla-battery/tesla-battery.module.ts
@@ -0,0 +1,35 @@
+/*
+ * tesla-battery.module.ts
+ */
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// services
+import { BatteryService } from './tesla-battery.service';
+// containers
+import { TeslaBatteryComponent } from './containers/tesla-battery/tesla-battery.component';
+
+// components
+import { TeslaCarComponent } from './components/tesla-car/tesla-car.component';
+
+@NgModule({
+  declarations: [
+    // registering our container component
+    TeslaBatteryComponent,
+    TeslaCarComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    // add the service to our sub-module
+    BatteryService
+  ],
+  exports: [
+    // exporting so our root module can access
+    TeslaBatteryComponent
+  ]
+})
+export class TeslaBatteryModule {}
